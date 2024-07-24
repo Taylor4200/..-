499d0c4c19f6e4c2499f69b996da0a3c3d7af74f
Hasheem <hasheemhush@gmail.com>
2024-07-24 13:38:30 +0530

    add listing

--- src/components/dashboard/add-property/AddPropertyBody.tsx
+++ src/components/dashboard/add-property/AddPropertyBody.tsx
@@ -8,10 +8,11 @@ import AddressAndLocation from "../profile/AddressAndLocation"
 import NiceSelect from "@/ui/NiceSelect";
 import {createClient} from "@/utils/supabase/client";
 import * as yup from "yup";
-import {useForm} from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
+import {Backdrop, CircularProgress} from "@mui/material";
 
 type state = {
     category: null | number
@@ -22,8 +23,10 @@ type state = {
 interface FormDataField {
     name: string;
     description?: string;
-    googlePlaceID: string
-    state: state[]
+    googlePlaceID?: string
+    state: state[],
+    lat?: string,
+    lang?: string
 }
 
 
@@ -35,8 +38,13 @@ const AddPropertyBody = () => {
         .object({
             name: yup.string().required().label("name"),
             // description: yup.string().required().label("description"),
-            googlePlaceID: yup.string().required().label("googlePlaceID"),
-            state: yup.array().min(1, "You must Select least 1 Category").required("required")
+            googlePlaceID: yup.string().label("googlePlaceID"),
+            state: yup.array().min(1, "You must Select least 1 Category").required("required"),
+            // lat: yup.number()
+            //     .nullable()
+            //     .transform(value => (value === '' ? null : value))
+            //     .notRequired()
+            // lang: yup.number().nullable().default(null),
 
             //     yup.object().shape({
             //    category: yup.number().required("Please Select a category"),
@@ -49,21 +57,35 @@ const AddPropertyBody = () => {
         handleSubmit,
         reset,
         setValue,
+        control,
         formState: {errors},
-    } = useForm<FormDataField>({resolver: yupResolver(schema),});
+    } = useForm<FormDataField>({
+        resolver: yupResolver(schema), defaultValues: {
+            lat: "",
+            lang: ""
+        }
+    });
 
     const [isLoading, setIsLoading] = useState(true)
     const [categories, setCategories] = useState<any>([])
 
+    const scrollTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     const onSubmit = async (data: FormDataField) => {
-
-        try{
-            console.log(data)
-
+        try {
+            setIsLoading(true)
             // Start a transaction
-            const { data: listing, error: listingError } = await supabase
+            const {data: listing, error: listingError} = await supabase
                 .from('Listing')
-                .insert({name: data.name, description: data?.description || ""})
+                .insert({
+                    name: data.name,
+                    description: data?.description || "",
+                    lat: data?.lat ? parseFloat(data?.lat) : null,
+                    lng: data?.lang ? parseFloat(data?.lang) : null,
+                    google_place_id: data?.googlePlaceID || ""
+                })
                 .select()
                 .single()  // Retrieve the inserted listing data including the generated listing_id
 
@@ -72,14 +94,14 @@ const AddPropertyBody = () => {
                 return
             }
 
-            console.log("list added",listing )
+            console.log("list added", listing)
             const listingId = listing.id
 
             data?.state?.map(async item => {
-                const { data: listingSubCategory, error: listingSubCategoryError } = await supabase
+                const {data: listingSubCategory, error: listingSubCategoryError} = await supabase
                     .from('listingsubcategories')
                     .insert([
-                        { listid: listingId, subcategoryid: item.subCategory }
+                        {listid: listingId, subcategoryid: item.subCategory}
                     ])
 
                 if (listingSubCategoryError) {
@@ -87,23 +109,27 @@ const AddPropertyBody = () => {
                     return
                 }
 
-                console.log('Successfully inserted listing and linked subcategory', { listing, listingSubCategory })
+                console.log('Successfully inserted listing and linked subcategory', {listing, listingSubCategory})
             })
 
+            scrollTop()
 
             const notify = () => toast('New Listing Created', {position: 'top-center'});
             notify();
             reset()
             setSelectedServices([])
 
-        } catch (e){
+        } catch (e) {
             console.log(e)
+        } finally {
+            setIsLoading(false)
         }
 
 
     }
 
     useEffect(() => {
+        setIsLoading(true)
         const fetchPosts = async () => {
             const {data} = await supabase.from('Categories').select(`
   id, 
@@ -141,13 +167,20 @@ const AddPropertyBody = () => {
 
     const handleDelete = (index: number) => setSelectedServices(prevState => prevState?.filter((_, i) => i !== index))
 
-
     useEffect(() => {
         setValue("state", selectedServices)
     }, [selectedServices]);
 
     return (
         <div className="dashboard-body">
+
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={isLoading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+
             <DashboardHeaderTwo title="Add New Listing"/>
             <form onSubmit={handleSubmit(onSubmit)} className="position-relative">
                 <h2 className="main-title d-block d-lg-none">Add New Listing</h2>
@@ -221,7 +254,7 @@ const AddPropertyBody = () => {
                         {/*</div>*/}
                         <div className="col-md-6">
                             <div className="dash-input-wrapper mb-30">
-                                <label htmlFor="">Google Place ID*</label>
+                                <label htmlFor="">Google Place ID</label>
                                 <input type="text" {...register("googlePlaceID")} placeholder="Enter Google Place ID"/>
                                 <p className="form_error">{errors.googlePlaceID?.message}</p>
                             </div>
@@ -243,6 +276,45 @@ const AddPropertyBody = () => {
 
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        {/*<div className="col-md-6">*/}
+                        {/*   <div className="dash-input-wrapper mb-30">*/}
+                        {/*      <label htmlFor="">Yearly Tax Rate*</label>*/}
+                        {/*      <input type="text" placeholder="Tax Rate" />*/}
+                        {/*   </div>*/}
+                        {/*</div>*/}
+                    </div>
+                    <div className="row align-items-end">
+                        <div className="col-md-6">
+                            <div className="dash-input-wrapper mb-30">
+                                <label htmlFor="">Latitude</label>
+
+                                <Controller
+                                    name="lat"
+                                    control={control}
+                                    render={({field}) => (
+                                        <input {...field} type="number" placeholder="Enter Latitude"/>
+                                    )}
+                                />
+
+
+                                <p className="form_error">{errors.lat?.message}</p>
+                            </div>
+
+
+                        </div>
+                        <div className="col-md-6">
+                            <div className="dash-input-wrapper mb-30">
+                                <label htmlFor="">Longitude</label>
+                                <Controller
+                                    name="lang"
+                                    control={control}
+                                    render={({field}) => (
+                                        <input {...field} type="number" placeholder="Enter Longitude"/>
+                                    )}
+                                />
+                                <p className="form_error">{errors.lang?.message}</p>
                             </div>
                         </div>
                         {/*<div className="col-md-6">*/}
@@ -279,7 +351,7 @@ const AddPropertyBody = () => {
                     <small>Upload file .jpg, .png, .mp4</small>
                 </div>
                 {/*<SelectAmenities />*/}
-                <AddressAndLocation/>
+                {/*<AddressAndLocation/>*/}
 
                 <div className="button-group d-inline-flex align-items-center mt-30">
                     <button type="submit" className="dash-btn-two tran3s me-3">Submit Listing</button>
