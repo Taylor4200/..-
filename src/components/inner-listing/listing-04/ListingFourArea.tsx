230d3b9a4ba2901ed6fae8ca6d9edcc52809600f
Hasheem <hasheemhush@gmail.com>
2024-08-01 12:20:30 +0530

    google map updates

--- src/components/inner-listing/listing-04/ListingFourArea.tsx
+++ src/components/inner-listing/listing-04/ListingFourArea.tsx
@@ -69,7 +69,7 @@ const ListingFourArea = ({data}: any) => {
         return {
             id: item.id,
             name: item?.name,
-            position: {lat: item?.lat, lng: item?.lng}
+            position: {lat: item?.lat, lng: item?.lng},
         }
     })
 
@@ -89,8 +89,224 @@ const ListingFourArea = ({data}: any) => {
     };
 
     const {isLoaded} = useLoadScript({
-        googleMapsApiKey: process?.env?.NEXT_PUBLIC_GOOGLEAPIKEY || ""
+        googleMapsApiKey: process?.env?.NEXT_PUBLIC_GOOGLEAPIKEY || "",
     });
+
+    const exampleMapStyles = [
+        {
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#ebe3cd",
+                },
+            ],
+        },
+        {
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#523735",
+                },
+            ],
+        },
+        {
+            elementType: "labels.text.stroke",
+            stylers: [
+                {
+                    color: "#f5f1e6",
+                },
+            ],
+        },
+        {
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#c9b2a6",
+                },
+            ],
+        },
+        {
+            featureType: "administrative.land_parcel",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#dcd2be",
+                },
+            ],
+        },
+        {
+            featureType: "administrative.land_parcel",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#ae9e90",
+                },
+            ],
+        },
+        {
+            featureType: "landscape.natural",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
+        },
+        {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
+        },
+        {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#93817c",
+                },
+            ],
+        },
+        {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [
+                {
+                    color: "#a5b076",
+                },
+            ],
+        },
+        {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#447530",
+                },
+            ],
+        },
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f5f1e6",
+                },
+            ],
+        },
+        {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#fdfcf8",
+                },
+            ],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f8c967",
+                },
+            ],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#e9bc62",
+                },
+            ],
+        },
+        {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#e98d58",
+                },
+            ],
+        },
+        {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#db8555",
+                },
+            ],
+        },
+        {
+            featureType: "road.local",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#806b63",
+                },
+            ],
+        },
+        {
+            featureType: "transit.line",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
+        },
+        {
+            featureType: "transit.line",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#8f7d77",
+                },
+            ],
+        },
+        {
+            featureType: "transit.line",
+            elementType: "labels.text.stroke",
+            stylers: [
+                {
+                    color: "#ebe3cd",
+                },
+            ],
+        },
+        {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
+        },
+        {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+                {
+                    color: "#b9d3c2",
+                },
+            ],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#92998d",
+                },
+            ],
+        },
+    ]
 
     return (
         <div className="property-listing-six bg-pink-two pt-110 md-pt-80 pb-170 xl-pb-120 mt-150 xl-mt-120">
@@ -161,7 +377,10 @@ const ListingFourArea = ({data}: any) => {
                             <div id="" className="h-100">
                             {
                                 isLoaded ?
-                                    <GoogleMap
+                                    <GoogleMap options={{
+                                        mapId: process?.env?.NEXT_PUBLIC_GOOGLEID || "",
+                                        // styles: exampleMapStyles,
+                                    }}
                                         onLoad={handleOnLoad}
                                         onClick={() => setActiveMarker(null)}
                                         mapContainerStyle={{height: "100vh"}}
