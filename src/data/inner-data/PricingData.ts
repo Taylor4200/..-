a2a2a7864792398570656dae470349cd16d2cf93
Taylor4200 <Taylorbonneville@gmail.com>
2024-08-06 22:51:58 -0600

    updated entire pricing sheet, cleaned code aswell

--- src/data/inner-data/PricingData.ts
+++ src/data/inner-data/PricingData.ts
@@ -20,29 +20,50 @@ const pricing_data: DataType[] = [
       pricing_data: [
          {
             id: 1,
-            class_name: "active",
+            // class_name: "active",
             plan: "Standard",
             price: "$185",
             desc: "Basic Listing",
-            icon_details: [{ icon: "fa-sharp fa-regular fa-check" }, { icon: "fa-sharp fa-regular fa-check" }, { icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "active"
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
          },
          {
             id: 2,
             plan: "Premium",
             price: "$325",
             desc: "Enhanced Visibility",
-            icon_details: [{ icon: "fa-sharp fa-regular fa-check" }, { icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "Get Started"
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
          },
          {
             id: 3,
             plan: "Pro",
             price: "$485",
             desc: "Maximum Exposure",
-            icon_details: [{ icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "Get Started"
-         },
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
+         }
       ]
    },
    {
@@ -50,31 +71,52 @@ const pricing_data: DataType[] = [
       pricing_data: [
          {
             id: 1,
-            class_name: "active",
+            // class_name: "active",
             plan: "Standard",
             price: "$185",
             desc: "Basic Listing",
-            icon_details: [{ icon: "fa-sharp fa-regular fa-xmark" }, { icon: "fa-sharp fa-regular fa-xmark" }, { icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "active"
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
          },
          {
             id: 2,
             plan: "Premium",
             price: "$325",
             desc: "Enhanced Visibility",
-            icon_details: [{ icon: "fa-sharp fa-regular fa-xmark" }, { icon: "fa-sharp fa-regular fa-xmark" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "Get Started"
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-xmark" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
          },
          {
             id: 3,
             plan: "Pro",
             price: "$485",
             desc: "Maximum Exposure",
-            icon_details: [{ icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" }, { icon_class: "available", icon: "fa-sharp fa-regular fa-check" },],
-            btn: "Get Started"
-         },
+            icon_details: [
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" },
+               { icon: "fa-sharp fa-regular fa-check" }
+            ],
+            btn: "Contact"
+         }
       ]
-   },
+   }
 ];
 
 export default pricing_data;
